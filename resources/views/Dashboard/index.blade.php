430d3e77517773196302ac7fbc160e744111be46
YosephFM <yosephfelixmarchello_2327240065@mhs.mdp.ac.id>
2025-06-11 19:37:32 +0700

    Update index.blade.php

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -6,42 +6,42 @@
 <table>
     <tr>
         <th class="p-2">
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 14rem;">
                 <img src="{{asset('images/aqua_1500_ml.png')}}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Aqua 1500ml</h5>
                     <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" class="btn btn-primary">Detail</a>
                 </div>
             </div>
         </th>
         <th class="p-3">
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 14rem;">
                 <img src="{{asset('images/aqua_cup_200ml.png')}}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Aqua Cup</h5>
                     <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" class="btn btn-primary">Detail</a>
                 </div>
             </div>
         </th>
         <th class="p-3">
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 14rem;">
                 <img src="{{asset('images/aqua_600ml.png')}}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Aqua 600ml</h5>
                     <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" class="btn btn-primary">Detail</a>
                 </div>
             </div>
         </th>
         <th class="p-2">    
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 14rem;">
                 <img src="{{asset('images/aqua_220ml.png')}}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Aqua 220ml</h5>
                     <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" class="btn btn-primary">Detail</a>
                 </div>
             </div>
         </th>
