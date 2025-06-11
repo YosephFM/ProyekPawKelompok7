beef3903e0cd3dadc53026b6e099962da7afd495
YosephFM <yosephfelixmarchello_2327240065@mhs.mdp.ac.id>
2025-06-11 18:41:51 +0700

    Penambahan card body di dashboard

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -1,148 +1,51 @@
 @extends('layout.main')
-@section('title', 'Dashboard')
 @section('content')
-
-
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/non-cartesian-zoom.js"></script>
-<script src="https://code.highcharts.com/modules/mouse-wheel-zoom.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script>
-
-<figure class="highcharts-figure">
-    <div id="container"></div>
-</figure>
-
-<style>
-.highcharts-figure,
-.highcharts-data-table table {
-    min-width: 320px;
-    max-width: 800px;
-    margin: 1em auto;
-}
-
-.highcharts-data-table table {
-    font-family: Verdana, sans-serif;
-    border-collapse: collapse;
-    border: 1px solid #ebebeb;
-    margin: 10px auto;
-    text-align: center;
-    width: 100%;
-    max-width: 500px;
-}
-
-.highcharts-data-table caption {
-    padding: 1em 0;
-    font-size: 1.2em;
-    color: #555;
-}
-
-.highcharts-data-table th {
-    font-weight: 600;
-    padding: 0.5em;
-}
-
-.highcharts-data-table td,
-.highcharts-data-table th,
-.highcharts-data-table caption {
-    padding: 0.5em;
-}
-
-.highcharts-data-table thead tr,
-.highcharts-data-table tbody tr:nth-child(even) {
-    background: #f8f8f8;
-}
-
-.highcharts-data-table tr:hover {
-    background: #f1f7ff;
-}
-
-input[type="number"] {
-    min-width: 50px;
-}
-
-.highcharts-description {
-    margin: 0.3rem 10px;
-}
-</style>
-
-<script>
-    Highcharts.chart('container', {
-    chart: {
-        type: 'pie',
-        zooming: {
-            type: 'xy'
-        },
-        panning: {
-            enabled: true,
-            type: 'xy'
-        },
-        panKey: 'shift'
-    },
-    title: {
-        text: 'Egg Yolk Composition'
-    },
-    tooltip: {
-        valueSuffix: '%'
-    },
-    subtitle: {
-        text:
-        'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>'
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: [{
-                enabled: true,
-                distance: 20
-            }, {
-                enabled: true,
-                distance: -40,
-                format: '{point.percentage:.1f}%',
-                style: {
-                    fontSize: '1.2em',
-                    textOutline: 'none',
-                    opacity: 0.7
-                },
-                filter: {
-                    operator: '>',
-                    property: 'percentage',
-                    value: 10
-                }
-            }]
-        }
-    },
-    series: [
-        {
-            name: 'Percentage',
-            colorByPoint: true,
-            data: [
-                {
-                    name: 'Water',
-                    y: 55.02
-                },
-                {
-                    name: 'Fat',
-                    sliced: true,
-                    selected: true,
-                    y: 26.71
-                },
-                {
-                    name: 'Carbohydrates',
-                    y: 1.09
-                },
-                {
-                    name: 'Protein',
-                    y: 15.5
-                },
-                {
-                    name: 'Ash',
-                    y: 1.68
-                }
-            ]
-        }
-    ]
-});
-</script>
+<marquee class="text-marqee" direction="left" scrollamount="15"> 
+          <h2 class="big-title">PT BINTANG SURYASINDO</h2>
+        </marquee>
+<table>
+    <tr>
+        <th class="p-2">
+            <div class="card" style="width: 18rem;">
+                <img src="{{asset('images/aqua_1500_ml.png')}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aqua 1500ml</h5>
+                    <br>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </th>
+        <th class="p-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{asset('images/aqua_cup_200ml.png')}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aqua Cup</h5>
+                    <br>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </th>
+        <th class="p-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{asset('images/aqua_600ml.png')}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aqua 600ml</h5>
+                    <br>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </th>
+        <th class="p-2">    
+            <div class="card" style="width: 18rem;">
+                <img src="{{asset('images/aqua_220ml.png')}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aqua 220ml</h5>
+                    <br>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </th>
+    </tr>
+</table>
 @endsection
+
